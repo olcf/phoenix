f941f71ca66228c1f43818210e48cfcef438e431
Matt Ezell <ezellma@ornl.gov>
2026-03-02 00:34:11 -0500

    Refactor ipxe script generation to use a jinja template

--- lib/phoenix/bootloader/ipxe.py
+++ lib/phoenix/bootloader/ipxe.py
@@ -4,54 +4,45 @@
 
 import logging
 from phoenix.bootloader import Bootloader
+from phoenix.node import Node
+import jinja2
+from jinja2 import Template
+
+ipxe_template = """
+{%- set server = 'http://' + http_server | default('${dhcp-server}') + ':' + http_server_port | default('8000')  -%}
+{%- if kcmdline is iterable and kcmdline is not string %}
+{%- set kcmdline = kcmdline | join(" ") -%}
+{%- endif -%}
+#!ipxe
+kernel {{server}}/images/{{image}}/vmlinuz initrd=initramfs.img root=live:{{server}}/images/{{image}}/rootdir.squashfs BOOTIF=${mac} ip=dhcp console={{console|default('ttyS0,115200')}} {{kcmdline}} || goto failed
+initrd {{server}}/images/{{image}}/initramfs.img || goto failed
+boot || goto failed
+
+:failed
+echo
+echo Boot must have failed
+echo
+echo Sleeping 30 seconds
+echo
+sleep 30
+echo Restarting networking
+ifclose
+ifconf
+echo
+chain {{server}}/bootfile
+
+"""
 
 class IpxeBootloader(Bootloader):
     bootloadertype = 'ipxe'
+    def_template = Node.environment.from_string(ipxe_template)
 
     @classmethod
     def script(cls, node):
         logging.debug("Generating iPXE script for node %s", node['name'])
 
-        try:
-            image = node['image']
-        except KeyError:
-            logging.warn('No image set for %s, not generating bootfile', node['name'])
-            raise
+        if 'image' not in node:
+            raise KeyError('No image set for %s, not generating bootfile' % node['name'])
 
-        try:
-            server_ip = node['http_server']
-        except KeyError:
-            server_ip = '${dhcp-server}'
-
-        try:
-            server_port = node['http_server_port']
-        except KeyError:
-            server_port = 8000
-
-        try:
-            console = node['console']
-        except KeyError:
-            console = 'ttyS0,115200'
-
-        try:
-            kcmdline = node['kcmdline']
-        except KeyError:
-            kcmdline = ''
-
-        default_args = "BOOTIF=${mac} ip=${ip}:%s-%d:${gateway}:${netmask}:${hostname} rw" % (server_ip, server_port)
-
-        result = list()
-        result.append('#!ipxe')
-        result.append('kernel http://%s:%d/images/%s/vmlinuz initrd=initramfs.gz %s console=%s %s' %
-                            (server_ip, server_port, node['image'], default_args, console, kcmdline))
-        result.append('initrd http://%s:%d/images/%s/initramfs.gz' % (server_ip, server_port, node['image']))
-        result.append('boot ||')
-        result.append('echo')
-        result.append('echo Boot must have failed')
-        result.append('echo')
-        result.append('echo Sleeping 30 seconds')
-        result.append('sleep 30')
-        result.append('chain http://%s:%d/bootfile' % (server_ip, server_port))
-        result.append('')
-
-        return '\n'.join(result)
+        script = cls.def_template.render({'node': node})
+        return script
